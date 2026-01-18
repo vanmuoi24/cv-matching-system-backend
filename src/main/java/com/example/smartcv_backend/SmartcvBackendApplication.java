48b38d2c950ae10f8b1807b2f4dc2ab2da7a1e4d
Tèn <heymac@Ten.local>
2026-01-18 20:45:11 +0700

    muoi

---
+++ src/main/java/com/example/smartcv_backend/SmartcvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartcv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartcvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartcvBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smartcv_backend/SmartcvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartcv_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcvBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
