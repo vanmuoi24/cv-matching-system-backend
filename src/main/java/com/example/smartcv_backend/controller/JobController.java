52e2069b91d0f32a046957ef64bd670dbbe42ae4
vanmuoi24 <domuoigghh@gmail.com>
2026-01-31 08:40:26 +0700

    crud

---
+++ src/main/java/com/example/smartcv_backend/controller/JobController.java
@@ -0,0 +1,49 @@
+package com.example.smartcv_backend.controller;
+
+import com.example.smartcv_backend.dto.request.JobCreateRequest;
+import com.example.smartcv_backend.dto.request.JobUpdateRequest;
+import com.example.smartcv_backend.dto.response.ApiResponse;
+import com.example.smartcv_backend.dto.response.JobInfoResponse;
+import com.example.smartcv_backend.dto.response.JobResponse;
+import com.example.smartcv_backend.service.JobService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jobs")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class JobController {
+
+    JobService jobService;
+
+    @PostMapping
+    ApiResponse<JobResponse> createJob(@RequestBody JobCreateRequest request) {
+        return ApiResponse.<JobResponse>builder().result(jobService.createJob(request)).build();
+    }
+
+    @GetMapping("/list")
+    public ApiResponse<List<JobResponse>> listJobs() {
+        return ApiResponse.<List<JobResponse>>builder().result(jobService.getAllJob()).build();
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<JobInfoResponse> getJobById(@PathVariable Long id) {
+        return ApiResponse.<JobInfoResponse>builder().result(jobService.getJobById(id)).build();
+    }
+
+    @PostMapping("/update/{id}")
+    public ApiResponse<JobResponse> updateJob(@PathVariable Long id, @RequestBody JobUpdateRequest request) {
+        return ApiResponse.<JobResponse>builder().result(jobService.updateJob(id, request)).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ApiResponse<Void> deleteJob(@PathVariable Long id) {
+        jobService.deleteJob(id);
+        return ApiResponse.<Void>builder().build();
+    }
+}
